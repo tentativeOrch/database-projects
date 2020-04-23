41810d16ded6a2935aa2ec282b6ce762b20d507f
mike1 <mike1@LAPTOP-F1ATBPTT>
2020-04-23 10:49:12 -0500

    first commit

---
+++ tentativeBot/src/main/java/tentativeBot/Bot.java
@@ -0,0 +1,19 @@
+package tentativeBot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+
+public class Bot {
+
+	public static void main(String[] args) throws LoginException {
+		
+		@SuppressWarnings("deprecation")
+		JDA jda = new JDABuilder("NjQ4MzUyNTExOTUwMDYxNTcw.XqCPMQ.-4W-NUsiQYsFJKOfsIYCGgkgULs").build();
+		jda.addEventListener(new MyListener());
+
+	}
+
+}
---
+++ tentativeBot/src/main/java/tentativeBot/MyListener.java
@@ -0,0 +1,12 @@
+package tentativeBot;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MyListener extends ListenerAdapter {
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		System.out.println(event.getMessage().getContentDisplay());
+		System.out.println(event.getMessage().getId());
+	}
+}
